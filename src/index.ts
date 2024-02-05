19eb392ed4a5fd934610fbb89b0ac4cbb129f377
narayaabinnadhkari <narayanabin223@gmail.com>
2024-02-05 23:27:14 +0545

    User Login API, auth-routes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
-
+import authRoutes from "./routes/auth"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 
